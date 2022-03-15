12b94061b4e98edd377ffb92f572cf15dae7cdfa
Maharjun Meesala <meesala.maharjun@byjus.com>
2022-03-15 17:01:30 +0530

    initiated the project with webpack, react and typescript

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+    console.log("here its coming ----")
+    return <h1>My React and TypeScript App!</h1>
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
